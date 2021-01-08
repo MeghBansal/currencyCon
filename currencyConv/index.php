7e9ae89abd2fbf314d0c19e0fd128269c25dd015
Meghdoot Bansal <Bansal.megh4@gmail.com>
2021-01-08 12:02:16 +0530

    Update index.php

--- currencyConv/index.php
+++ currencyConv/index.php
@@ -2,28 +2,6 @@
 $api_url = 'https://api.exchangeratesapi.io/latest';
 $json_data = file_get_contents($api_url);
 $response = json_decode($json_data, true);
- 
-$amount = 1;
-$target_amt = 1;
-$selected_base = "CAD";
-$selected_target  = "CAD";
-if(isset($_POST['submit'])){
-    $selected_base = $_POST['base'];
-    $amount = $_POST['number'];
-    $selected_target = $_POST['target'];
-
-    $url_target ='https://api.exchangeratesapi.io/latest?base='.$selected_base.'';
-    $new_json_data = file_get_contents($url_target);
-    $new_response = json_decode($new_json_data, true);
-    //$new_response["rates"] = $new_response;
-
-    foreach($new_response["rates"] as $key => $value){
-        if($key == "$selected_target"){
-            $target_amt = $value;
-        }
-    }
-
-}
 
 ?>
 <!DOCTYPE html>
@@ -31,8 +9,66 @@ if(isset($_POST['submit'])){
 <head>
 	<title>Currency</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"></link>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            document.getElementById("baseAmount").defaultValue = "1";
+            document.getElementById("targetAmount").defaultValue = "1";
+
+        $("#base").change(function(){
+            var selectedBase = $('#base').val(); 
+            var targetBase = $("#target").val();
+            var baseAmount = $("#baseAmount").val();
+
+            url =`https://api.exchangeratesapi.io/latest?base=${selectedBase}`;
+            $.getJSON(url,(item)=>{
+                $(function() {
+                    $("#targetAmount").val(item.rates[targetBase]*baseAmount);
+                });
+            });
+        })
+        $("#target").change(function(){
+            var selectedBase = $('#base').val(); 
+            var targetBase = $("#target").val();
+            var targetAmount = $("#targetAmount").val();
+            //var baseAmount = $("#baseAmount").val();
+            
+            url =`https://api.exchangeratesapi.io/latest?base=${targetBase}`;
+            $.getJSON(url,(item)=>{
+                $(function() {
+                    $("#baseAmount").val(item.rates[selectedBase]*targetAmount);
+                });
+            });
+        })
+        $("#baseAmount").on("input",function(){
+            var selectedBase = $('#base').val(); 
+            var targetBase = $("#target").val();
+            //var targetAmount = $("#targetAmount").val();
+            var baseAmount = $("#baseAmount").val();
+            
+            url =`https://api.exchangeratesapi.io/latest?base=${selectedBase}`;
+            $.getJSON(url,(item)=>{
+                $(function() {
+                    $("#targetAmount").val(item.rates[targetBase]*baseAmount);
+                });
+            });
+        })
+        $("#targetAmount").on("input",function(){
+            var selectedBase = $('#base').val(); 
+            var targetBase = $("#target").val();
+            var targetAmount = $("#targetAmount").val();
+            //var baseAmount = $("#baseAmount").val();
+            
+            url =`https://api.exchangeratesapi.io/latest?base=${targetBase}`;
+            $.getJSON(url,(item)=>{
+                $(function() {
+                    $("#baseAmount").val(item.rates[selectedBase]*targetAmount);
+                });
+            });
+        })
+    });
+      </script>
 </head>
 <body>
     <div class="container">
@@ -42,9 +78,9 @@ if(isset($_POST['submit'])){
             <div class="card-body ">
                 <h4 class="card-title text-center pb-2">Choose Base Currency</h4>
                 <form method="post">
-                    <div class="row mb-5 justify-content-between">
+                    <div class="row mb-2 justify-content-between">
                         <div class="col-md-5">
-                            <input type="number" name="number" class="form-control" step="0.01" min="1" value=<?php echo $amount ?>>
+                            <input type="number" placeholder="1" id="baseAmount" class="form-control" min="1" value=''>
                         </div>
                         <div class="col-md-5 ">
                             <select id="base" name="base" class="form-control">
@@ -57,14 +93,14 @@ if(isset($_POST['submit'])){
                         </div>
                     </div>
                     <hr />
-                    <h4 class="card-title text-center">Choose Target Currency</h4>
+                    <h4 class=" pt-2 pb-2 card-title text-center">Choose Target Currency</h4>
 
                     <div class="row justify-content-between">
                         <div class="col-md-5">
-                            <input type="number" class="form-control" min="0" step="0.01" value=<?php echo $target_amt*$amount ?>>
+                            <input type="number" id="targetAmount" class="form-control" placeholder="1" min="1" value=''>
                         </div>
                         <div class="col-md-5 ">
-                            <select name="target" class="form-control">
+                            <select id="target" name="target" class="form-control">
 
                                 <?php foreach($response["rates"] as $key => $value){ ?>
                                     <?php echo "<option value='$key'> $key </option>"?>
@@ -73,7 +109,6 @@ if(isset($_POST['submit'])){
                             </select>
                         </div>
                     </div>
-                    <input type="submit" name="submit" class=" mt-5 btn btn-block btn-primary"></input>
                 </form>
             </div>
         </div>
